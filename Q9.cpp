dc33c72feb1cc3664fc81592feb78526f730afdb
snehdeep047 <snehdeep25csu233@ncuindia.edu>
2026-05-09 12:40:54 +0530

    Add files via upload

---
+++ Q9.cpp
@@ -0,0 +1,43 @@
+/*Q9. A text editor auto-detects whether an input letter is a vowel, a consonant or
+ a number. Implement a solution to classify the symbol. */
+
+#include <iostream>
+using namespace std;
+
+class Alphabet
+{
+    public :
+
+    void comp(){
+    char alpha;
+
+    cout << "Enter Your alphabet (A-Z) : ";
+    cin >> alpha;
+
+    switch (alpha)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        cout << "Vowel";
+        break;
+
+    default:
+        cout << "Consonant";
+    }
+}
+};
+
+int main (){
+    Alphabet obj;
+    obj.comp();
+
+    return 0;
+}
